4b8be3b0e4871e192055de055176ef6c842d0ac1
Lucas Rodrigues <lucasro229@gmail.com>
2023-02-06 00:49:27 -0300

    Update AppController
    Co-authored-by: Kleidson Alves      <kleidsonalves15@gmail.com>

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+/*Define a controller da aplicação */
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  /*rota padrão da aplicação */
+  /*Espera-se como retorno a string hello world */
   @Get()
   getHello(): string {
     return this.appService.getHello();
